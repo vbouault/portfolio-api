0e1178198cee796189a0b325ce81a5585a75d2fe
vbouault <vbouault@hotmail.fr>
2020-08-27 12:56:59 +0200

    test cors

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000);
 
 // middlewares
 app.use(express.json());
-// app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 app.use('/contacts', require('./routes/contact.routes.js'));
 app.use('/', (req, res) => { res.redirect('/contacts'); });
