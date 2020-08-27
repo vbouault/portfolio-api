5083b8d61dfcec8198c57ff627a18703d0bc732c
vbouault <vbouault@hotmail.fr>
2020-08-27 12:46:14 +0200

    add Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000)
 // middlewares
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/contacts', require('./routes/contact.routes.js'));
 app.use('/', (req, res) => { res.redirect('/contacts'); });
