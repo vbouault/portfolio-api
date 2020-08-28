cd740f175835a4e8bc7c668741cb3609e8bc7b10
vbouault <vbouault@hotmail.fr>
2020-08-28 15:43:28 +0200

    test cors

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000)
 // middlewares
 app.use(express.json());
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
 app.use('/contacts', require('./routes/contact.routes.js'));
 app.use('/', (req, res) => { res.redirect('/contacts'); });
