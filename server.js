fb4eb791e997de9f371bd5f32ca29f762686f56c
vbouault <vbouault@hotmail.fr>
2020-08-28 15:30:31 +0200

    test cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || (process.env.NODE_ENV === 'test' ? 3001 : 3000);
 
 // middlewares
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With");
